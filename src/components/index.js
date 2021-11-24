53baa2aa5fc66ebea2c1418c6658d1b04bbdc283
Mhmd <ichigo_2002z@hotmail.com>
2021-11-24 03:10:26 +0200

    CRM Starter

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Loader} from './Loader';
+export {default as MessageView} from './MessageView';
+
+
+export {default as InfoView} from './InfoView';
+export {default as GridContainer} from './GridContainer';
